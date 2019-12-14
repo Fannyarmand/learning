f00790a86d4ca3c3095c79645f1ad18392bf6bbe
Fanny Armand <fannyarmand2@gmail.com>
2019-12-14 14:21:57 +0100

    ma première version

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi");
+const howAreYouString = 'how are you?'
+const friends = ['Romain', 'Fanny','PP','Pierre', 'JC']
+
+friends.forEach(hello)
+console.log(howAreYouString);
+
+///////// My functions ////////
+
+function hello(name) {
+  console.log(`hello ${name}`);
+}
