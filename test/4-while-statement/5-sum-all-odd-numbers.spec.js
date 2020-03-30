e3ab9ffab502864b0a7650e8ee0b4435b8cdb174
merlin <romain.dary@renault.com>
2020-03-30 10:48:20 +0200

    add lessons

---
+++ test/4-while-statement/5-sum-all-odd-numbers.spec.js
@@ -0,0 +1,16 @@
+const should = require('should');
+const { describe, it } = require('mocha');
+
+const sumAllOddNumbers = require('../../lessons/4-while-statement/5-sum-all-odd-numbers');
+
+describe('SumAllOddNumbers', () => {
+  it('should be a function', () => {
+    should(sumAllOddNumbers).be.of.type('function', '"sumAllOddNumbers" must be a function');
+  });
+
+  it('should sumAllOddNumbers', () => {
+    sumAllOddNumbers(0).should.be.equal(0);
+    sumAllOddNumbers(1000).should.be.equal(250500);
+    sumAllOddNumbers(100000).should.be.equal(2500050000);
+  });
+});
