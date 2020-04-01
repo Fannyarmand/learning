5b04ba4892d62b32b58b409ea48db9a2b816f607
merlin <romain.dary@renault.com>
2020-04-01 15:21:42 +0200

    add cube ascii

---
+++ lessons/4-while-statement/5-sum-all-odd-numbers.js
@@ -0,0 +1,8 @@
+// Exercise: Sum all odd numbers from 0 to n
+//
+// Change arguments if necessary
+function sumAllOddNumbers(arg) {
+  // Write your code here
+}
+
+module.exports = sumAllOddNumbers;
