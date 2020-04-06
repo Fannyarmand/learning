8938fb15516b3475997a7dcca341d21a982cf961
merlin <romain.dary@renault.com>
2020-04-06 09:21:25 +0200

    add space invader

--- test/4-while-statement/12-generate-grey-space-invader.spec.js
+++ test/4-while-statement/12-generate-grey-space-invader.spec.js
@@ -1,46 +1,25 @@
 const should = require('should');
 const { describe, it } = require('mocha');
 
-const generateCube = require('../../lessons/4-while-statement/10-generate-cube');
+const generateGreySpaceInvader = require('../../lessons/4-while-statement/12-generate-grey-space-invader');
 
-describe('GenerateCube', () => {
+describe('GenerateGreySpaceInvader', () => {
   it('should be a function', () => {
-    should(generateCube).be.of.type('function', '"generateCube" must be a function');
+    should(generateGreySpaceInvader).be.of.type('function', '"generateGreySpaceInvader" must be a function');
   });
 
-  it('should throw if not valid', () => {
-    should(() => generateCube(-1)).throw();
-    should(() => generateCube(100)).throw();
-    should(() => generateCube(21)).throw();
-  });
-
-  it('should generateCube small cube', () => {
-    generateCube(0).should.be.equal("+");
-    generateCube(1).should.be.equal("+");
-    generateCube(0,true).should.be.equal("+");
-    generateCube(1,true).should.be.equal("+");
-  });
-
-  it('should generateCube big cube', () => {
-    generateCube(8).should.be.equal(
-`+ + + + + + + +
-+             +
-+             +
-+             +
-+             +
-+             +
-+             +
-+ + + + + + + +`
+  it('should generateGreySpaceInvader big cube', () => {
+    generateGreySpaceInvader().should.be.equal(
+`░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░██░░░░░░░░░░██░░░░░░
+░░░░░░░░██░░░░░░██░░░░░░░░
+░░░░░░██████████████░░░░░░
+░░░░████░░██████░░████░░░░
+░░██████████████████████░░
+░░██░░██████████████░░██░░
+░░██░░██░░░░░░░░░░██░░██░░
+░░░░░░░░████░░████░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░`
     );
   });
-
-  it('should generateCube filled', () => {
-    generateCube(4, true).should.be.equal(
-`+ + + +
-+ o o +
-+ o o +
-+ + + +`
-    );
-  });
-
 });
